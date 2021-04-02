de719bde121ad72b32351527a765bd8e5adefde2
Drimetolle <drimetolle@gmail.com>
2021-04-02 14:43:31 +0500

    added jest

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  roots: ["<rootDir>"],
+  testMatch: [
+    "**/tests/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+};
