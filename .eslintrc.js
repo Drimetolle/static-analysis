0cf3ba8a9e8c2e82c23e34ff469ed45aaebd2fe5
Drimetolle <drimetolle@gmail.com>
2021-07-17 13:51:02 +0500

    Переход на новую версию линтера

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,10 @@
-// eslint-disable-next-line no-undef
 module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
   root: true,
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
+  extends: ["plugin:prettier/recommended"],
 };
