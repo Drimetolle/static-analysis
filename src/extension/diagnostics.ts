2b54cd747067a57154ffd3538c7e46212517fbbb
Drimetolle <drimetolle@gmail.com>
2021-10-05 18:00:33 +0500

    чистка

--- src/extension/diagnostics.ts
+++ src/extension/diagnostics.ts
@@ -11,14 +11,10 @@ import AllRules from "../rules";
 import FileManager from "../file-system/FileManager";
 import WalkersHelper from "../linter/walkers/WalkersHelper";
 import IssuesQueue from "../linter/issue/IssuesQueue";
-import Formatter from "../cli-engine/Formatter";
 import Controller from "../Controller";
 
 /** Code that is used to associate diagnostic entries with code actions. */
 export const EMOJI_MENTION = "emoji_mention";
-
-/** String to detect in the text document. */
-const EMOJI = "emoji";
 
 container.register<Linter>(Linter, {
   useValue: new Linter(AllRules, {
@@ -46,9 +42,6 @@ export async function refreshDiagnostics(
   await controller.run();
   const diagnostics: vscode.Diagnostic[] = [];
 
-  // for (let lineIndex = 0; lineIndex < doc.lineCount; lineIndex++) {
-  const lineOfText = doc.lineAt(103);
-  // if (lineOfText.text.includes(EMOJI)) {
   const test = container.resolve(IssuesQueue).issues[0];
   diagnostics.push(
     new vscode.Diagnostic(
@@ -56,70 +49,15 @@ export async function refreshDiagnostics(
         test.link.line - 1,
         test.link.start - 1,
         test.link.line - 1,
-        test.link.start + (test.line?.length ?? 1)
+        test.link.start + (test.line?.length ?? 1) - 1
       ),
-      "When you say 'emoji', do you want to find out more?",
-      vscode.DiagnosticSeverity.Information
+      test.description,
+      vscode.DiagnosticSeverity.Warning
     )
   );
-  console.log(
-    JSON.stringify(doc.lineAt(test.link.line - 1)),
-    test.link.line - 1,
-    
-  );
-  // }
-  // }
-
-  // for (const issue of container.resolve(IssuesQueue).issues) {
-  //   console.log(issue.link.start);
-  //   diagnostics.push(
-  //     createDiagnostic(doc, doc.lineAt(issue.link.start), issue.link.start)
-  //   );
-  // }
+  console.log(container.resolve(IssuesQueue));
 
   emojiDiagnostics.set(doc.uri, diagnostics);
-}
-
-// export function refreshDiagnostics(
-//   doc: vscode.TextDocument,
-//   emojiDiagnostics: vscode.DiagnosticCollection
-// ): void {
-//   const controller = container.resolve(Controller);
-//   const diagnostics: vscode.Diagnostic[] = [];
-//
-//   container.resolve(IssuesQueue).subscribe((i) => {
-//     diagnostics.push(createDiagnostic(doc, doc.lineAt(1), 1));
-//     console.log(Formatter.formatMessage(i));
-//   });
-//
-//   controller.run();
-//
-//   emojiDiagnostics.set(doc.uri, diagnostics);
-// }
-
-function createDiagnostic(
-  doc: vscode.TextDocument,
-  lineOfText: vscode.TextLine,
-  lineIndex: number
-): vscode.Diagnostic {
-  // find where in the line of thet the 'emoji' is mentioned
-  const index = lineOfText.text.indexOf(EMOJI);
-
-  // create range that represents, where in the document the word is
-  const range = new vscode.Range(
-    lineIndex,
-    index,
-    lineIndex,
-    index + EMOJI.length
-  );
-
-  const diagnostic = new vscode.Diagnostic(
-    range,
-    "When you say 'emoji', do you want to find out more?",
-    vscode.DiagnosticSeverity.Information
-  );
-  diagnostic.code = EMOJI_MENTION;
-  return diagnostic;
 }
 
 export function subscribeToDocumentChanges(
