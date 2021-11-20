723ec8a5d41c468b9eb6708d439dc2e1c7bc7b32
Drimetolle <drimetolle@gmail.com>
2021-11-20 18:21:08 +0500

    Done rule: ReturnForNotVoidFunction

--- src/rules/functions/ReturnForNotVoidFunction.ts
+++ src/rules/functions/ReturnForNotVoidFunction.ts
@@ -1,6 +1,11 @@
 import Rule from "../../linter/Rule";
 import LinterContext from "../../linter/LinterContext";
 import Report from "../../linter/issue/Report";
+import StartBlock from "../../source-analysis/control-flow/blocks/StartBlock";
+import FunctionBlock from "../../source-analysis/control-flow/blocks/FunctionBlock";
+import BasicBlock from "../../source-analysis/control-flow/blocks/BasicBlock";
+import ReturnBlock from "../../source-analysis/control-flow/blocks/ReturnBlock";
+import { TypeSpecifier } from "../../source-analysis/data-objects/LanguageKeyWords";
 
 /**
  * @example
@@ -12,6 +17,48 @@ import Report from "../../linter/issue/Report";
  */
 export default class ReturnForNotVoidFunction extends Rule {
   run(context: LinterContext): Array<Report> {
-    return [];
+    const result = new Array<Report>();
+    const functionBlocks = ReturnForNotVoidFunction.visitFunctionBlocks(
+      context.cfg
+    );
+
+    for (const functionBlock of functionBlocks) {
+      if (
+        functionBlock.type != TypeSpecifier.VOID &&
+        !ReturnForNotVoidFunction.hasReturnStatement(functionBlock)
+      ) {
+        result.push(
+          new Report(
+            `Function: ${functionBlock.functionDeclaration} doesn't have any return statement or have empty return statement.`,
+            functionBlock.ast
+          )
+        );
+      }
+    }
+
+    return result;
+  }
+
+  private static *visitFunctionBlocks(cfg: StartBlock) {
+    for (const child of cfg.blocks) {
+      if (child instanceof FunctionBlock) {
+        yield child;
+      }
+    }
+  }
+
+  private static hasReturnStatement(
+    cfg: BasicBlock,
+    result: { has: boolean } = { has: false }
+  ): boolean {
+    for (const block of cfg.blocks) {
+      if (block instanceof ReturnBlock && block.ast.text !== "return;") {
+        result.has = true;
+        return result.has;
+      }
+      ReturnForNotVoidFunction.hasReturnStatement(block, result);
+    }
+
+    return result.has;
   }
 }
