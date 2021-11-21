2a19859b982eb0271cd4d694233acc0f94cd3634
Drimetolle <drimetolle@gmail.com>
2021-11-21 14:09:07 +0500

    Fix rule: UnnamedFunctionParameters

--- tests/rules-tests/UnnamedFunctionParametersTests.ts
+++ tests/rules-tests/UnnamedFunctionParametersTests.ts
@@ -1,21 +1,31 @@
 ﻿import UnnamedFunctionParameters from "../../src/rules/functions/UnnamedFunctionParameters";
 import { createContextWithAst } from "../../utils-for-testing/LinterContextCreators";
 
-describe("Check declaration string in C like style", () => {
+describe("Check unnamed function parameters", () => {
   const functionWrapper = (code: string) => `main(${code}) {}`;
   const rule = new UnnamedFunctionParameters();
 
   test.each([
-    [`int`],
+    [`int`, `int`],
     [`const int`, `constint`],
-    [`int&`],
-    [`int*`],
-    [`int*&`],
+    [`int&`, `int&`],
+    [`int*`, `int*`],
+    [`int*&`, `int*&`],
     [`const int*`, `constint*`],
     [`const int&`, `constint&`],
-  ])(
+    [`char s[]`, undefined],
+    [`int a`, undefined],
+    [`int a[]`, undefined],
+    [`int *a`, undefined],
+    [`int &a`, undefined],
+    [`int &a[]`, undefined],
+    [`const int a`, undefined],
+    [`const SomeType a`, undefined],
+    // TODO unnamed parameter for custom type
+    // [`SomeType`, `SomeType`],
+  ] as Array<[string, string | undefined]>)(
     "using single unnamed function parameters %s",
-    async (code, expected = code) => {
+    async (code, expected) => {
       const result = rule.run(createContextWithAst(functionWrapper(code)));
 
       expect(result.pop()?.node.text).toBe(expected);
@@ -30,6 +40,12 @@ describe("Check declaration string in C like style", () => {
     [`int*&, float*&`, [`int*&`, `float*&`]],
     [`const int*, const float*`, [`constint*`, `constfloat*`]],
     [`const int&, const float&`, [`constint&`, `constfloat&`]],
+    [`int a, float b`, []],
+    [`int &a, float *b`, []],
+    [`const int &a, const float *b`, []],
+    [`const int &a[], const float *b[][]`, []],
+    // TODO unnamed parameter for custom type
+    // [`SomeType &a, SomeType *b`, [`SomeType&a`, `SomeType*b`]],
   ])(
     "using multiple unnamed function parameters %s",
     async (code, expected) => {
