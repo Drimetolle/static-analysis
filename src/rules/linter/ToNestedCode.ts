d8b5c74b8e435d58cc368b2e31a22cddf28d25c4
Drimetolle <drimetolle@gmail.com>
2021-11-28 15:04:13 +0500

    Buf fix in rule ToNestedCode

--- src/rules/linter/ToNestedCode.ts
+++ src/rules/linter/ToNestedCode.ts
@@ -3,13 +3,16 @@ import LinterContext from "../../linter/LinterContext";
 import Report from "../../linter/issue/Report";
 import BasicBlock from "../../source-analysis/control-flow/blocks/BasicBlock";
 import { ParserRuleContext } from "antlr4ts/ParserRuleContext";
+import IfBlock from "../../source-analysis/control-flow/blocks/IfBlock";
+import LoopBlock from "../../source-analysis/control-flow/blocks/LoopBlock";
+import SwitchBlock from "../../source-analysis/control-flow/blocks/switch/SwitchBlock";
 
 export default class ToNestedCode extends Rule {
   run(context: LinterContext): Array<Report> {
     const result = new Array<Report>();
     const blocks = this.findBlockWithDepthThree(context.cfg);
 
-    for (const block of blocks) {
+    for (const block of new Set(blocks)) {
       result.push(
         new Report(
           `Control flow statements "if", "for", "while", "switch" and "try" should not be nested too deeply`,
@@ -26,13 +29,21 @@ export default class ToNestedCode extends Rule {
     result: Array<ParserRuleContext> = []
   ): Array<ParserRuleContext> {
     for (const child of block.blocks) {
-      if (child.scopeDepth >= 2) {
-        result?.push(child.ast);
+      if (child.scopeDepth >= 2 && ToNestedCode.isFlowBlock(child)) {
+        result.push(child.ast);
       }
 
       this.findBlockWithDepthThree(child, result);
     }
 
     return result;
+  }
+
+  private static isFlowBlock(block: BasicBlock) {
+    return (
+      block instanceof IfBlock ||
+      block instanceof LoopBlock ||
+      block instanceof SwitchBlock
+    );
   }
 }
