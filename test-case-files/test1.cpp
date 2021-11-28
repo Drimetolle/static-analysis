9650582a9ebbf06d16d8e18a699e84a3a28f6787
Drimetolle <drimetolle@gmail.com>
2021-11-28 14:08:48 +0500

    Sample for rule CheckUserInput

--- test-case-files/test1.cpp
+++ test-case-files/test1.cpp
@@ -1,3 +1,14 @@
-void main() {
-    int *&var = 1;
+void main()
+{
+  int buff, a;
+  tests1(buff);
+  scanf("%d", buff, buff2);
+//  tests2(buff, b2);
+
+//  if (buff == 0 && buffs) {
+//    call();
+//  }
+
+//  a = buff;
+  a = buff, buff = 2;
 }
