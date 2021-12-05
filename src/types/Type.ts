6aeb7a3f5d481f72faf96531098fb4c812bfda6a
Drimetolle <drimetolle@gmail.com>
2021-12-05 15:39:56 +0500

    Interface for type

---
+++ src/types/Type.ts
@@ -0,0 +1,35 @@
+﻿import { TypeSpecifier } from "../source-analysis/data-objects/LanguageKeyWords";
+
+export default class Type {
+  public get fullName() {
+    return "";
+  }
+
+  public get IsPrimitive() {
+    return false;
+  }
+
+  public get IsArray() {
+    return false;
+  }
+
+  public get IsClass() {
+    return false;
+  }
+
+  public get IsStruct() {
+    return false;
+  }
+
+  public getPrimitiveType() {
+    return TypeSpecifier.INT;
+  }
+
+  public getNestedTypes(): Array<Type> {
+    return [];
+  }
+
+  public toString() {
+    return "";
+  }
+}
